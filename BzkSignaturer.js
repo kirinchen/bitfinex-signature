ce5c28ad1ee7e6dea9a8eea221dd59c7556b9d45
kirin <kirin.chen1001@gmail.com>
2020-11-28 10:22:40 +0800

    update bzk plugin

--- BzkSignaturer.js
+++ BzkSignaturer.js
@@ -1,38 +1,22 @@
 const Bridge = require('bzk/dist/Bridge').Bridge;
-const RpcObj = require('bzk/dist/dtos').RpcObj;
-const Uids = require('bzk/dist/dtos').Uids;
-const VarQuery = require('bzk/dist/dtos').VarQuery;
-const VarQueryPoint = require('bzk/dist/dtos').VarQueryPoint;
 const Signaturer = require('./Signaturer');
 
 let bridge = Bridge.getInstance();
 
-async function genSignatureByBridge() {
+function genSignatureByBridge() {
     const sg = new Signaturer();
-    sg.apiKey = await bridge.getVar('bitfinex.key').val;
-    sg.apiSecret = await bridge.getVar('bitfinex.secret').val;
-    sg.apiPath = await bridge.getVar('apiPath').val;
-    sg.body = await bridge.getVar('apiBody').val;
+    sg.apiKey =  bridge.get('bitfinex.key');
+    sg.apiSecret =  bridge.get('bitfinex.secret');
+    sg.apiPath =  bridge.get('apiPath');
+    sg.body =  bridge.get('apiBody');
     return sg;
 }
 
-async function insertSignatureToVar() {
-    const sg = await genSignatureByBridge();
+async function insertSignatureToVar(headerKey,urlKey) {
+    const sg = genSignatureByBridge();
     const h = sg.genSignHeader();
-    const rhlv = await bridge.getVar('rhlv').val;
-    const rhkey = await bridge.getVar('rhkey').val;
-    bridge.markVar({
-        lv: rlv,
-        key: rkey,
-        val: h
-    });
-    const rulv = await bridge.getVar('rulv').val;
-    const rukey = await bridge.getVar('rukey').val;
-    bridge.markVar({
-        lv: rulv,
-        key: rukey,
-        val: sg.getApiUrl()
-    });
+    bridge.set(headerKey,h);
+    bridge.set(urlKey,sg.getApiUrl());
 }
 
 module.exports = {
